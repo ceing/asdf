97a6a23876306f2c5b1a91f6a536d8ae75a3c2cd
ceing <ai39@163.com>
2021-08-31 10:44:12 +0800

    add test case

--- src/Aa.java
+++ src/Aa.java
@@ -1,1 +1,8 @@
-import j
+ public  class Aa {
+    @org.junit.jupiter.api.Test
+    @org.junit.jupiter.api.DisplayName("Test name")
+    void testName(){
+        
+        org.junit.jupiter.api.Assertions.fail("fail first");
+    }
+}
